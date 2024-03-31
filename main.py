d8292a73d2e1b347c5e1efa1fc07444a5e93429f
Eridani Alcantar <ealcantar@utexas.edu>
2024-03-31 00:39:11 -0500

    Modified UILResults to accept an int or a list containing year(s) to scrape.

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ contest = "CAL"
 conference = "5A"
 meet_level = "R"
 meet_div = 3
-year_start = 2017
-year_end = 2019
+years = [2017, 2019]
 
-results = UILResults(contest, conference, meet_level, meet_div, year_start, year_end)
+results = UILResults(contest, conference, meet_level, meet_div, years)
 results.agg_data()
 results.print_results()
 results.write_csv("results.csv")
