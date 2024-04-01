7821145234a7374c6b91e3a50722ee0e870bf0b9
Eridani Alcantar <ealcantar@utexas.edu>
2024-04-01 01:48:15 -0500

    Added try-except block for errors.

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from uil_results import UILResults
 
-contest = ["NUM", "CAL"]
-conference = ["5A", "6A"]
-meet_level = ["D", "R", "S"]
+contest = ["NUM"]
+conference = ["5A"]
+meet_level = ["D", "R"]
 district_nums = [i for i in range(1, 6)]
-meet_level_num = {
-    "R": [2, 3],
-    "D": district_nums,
-}
-years = [2017, 2018]
+meet_level_num = {"R": [2, 3], "D": [-1, 1, 2]}
+years = [1993, 2017, 2018]
 
 results = UILResults(contest, conference, meet_level, meet_level_num, years)
 results.agg_data()
--- uil_extractor.py
+++ uil_extractor.py
@@ -27,32 +27,38 @@ class UILExtractor:
             "rows": None,
         }
 
-    def extract_individual_results(self, meet_level: str):
+    def extract_individual_results(self):
         self.data["rows"] = []
         """Extract individual contestant results from HTML file"""
         soup = BeautifulSoup(self.scraper.html, "html.parser")
         # Find first table and get all table rows
-        for index, tr in enumerate(soup.table.findAll("tr")):
-            row = []
-            # Find all table data cells
-            for td in tr.findAll("td"):
-                # Convert object to string and strip whitespace characters
-                data_string = str(td.string).strip()
-                row.append(data_string)
-            if index != 0:
-                # Add extra element if results are from state
-                if meet_level == "S":
-                    row.insert(6, "")
-                self.data["rows"].append(
-                    row
-                    + [
-                        self.scraper.meet_level,
-                        self.scraper.level_num,
-                        self.scraper.year,
-                        self.scraper.event,
-                        self.scraper.conference,
-                    ]
-                )
+        try:
+            for index, tr in enumerate(soup.table.findAll("tr")):
+                row = []
+                # Find all table data cells
+                for td in tr.findAll("td"):
+                    # Convert object to string and strip whitespace characters
+                    data_string = str(td.string).strip()
+                    row.append(data_string)
+                if index != 0:
+                    # Add extra element if results are from state
+                    if self.scraper.meet_level == "S":
+                        row.insert(6, "")
+                    self.data["rows"].append(
+                        row
+                        + [
+                            self.scraper.meet_level,
+                            self.scraper.level_num,
+                            self.scraper.year,
+                            self.scraper.event,
+                            self.scraper.conference,
+                        ]
+                    )
+        except AttributeError:
+            # TODO Decide on consistent naming for parameters in all modules.
+            print(
+                f"No table found for {self.scraper.year, self.scraper.conference, self.scraper.event, self.scraper.meet_level, self.scraper.level_num}"
+            )
 
     def write_csv(self, filepath: str) -> None:
         """Write extracted data into a csv file"""
