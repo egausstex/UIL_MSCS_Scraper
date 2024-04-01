31975903ce239e2b5e852ac339a2812655462c08
Eridani Alcantar <ealcantar@utexas.edu>
2024-04-01 17:50:48 -0500

    scrape Math and CS contests.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from uil_results import UILResults
 
-contest = ["NUM"]
-conference = ["5A"]
-meet_level = ["D", "R"]
-district_nums = [i for i in range(1, 6)]
-meet_level_num = {"R": [2, 3], "D": [-1, 1, 2]}
-years = [1993, 2017, 2018]
+years = [i for i in range(2005, 2023)]
+contest = ["NUM", "CAL", "MTH", "CSC"]
+conference = ["1A", "2A", "3A", "4A", "5A"]
+meet_level = ["D", "R", "S"]
+district_nums = [i for i in range(1, 35)]
+meet_level_num = {"R": [1, 2, 3, 4], "D": district_nums}
 
-results = UILResults(contest, conference, meet_level, meet_level_num, years)
-results.agg_data()
-results.print_results()
-results.write_csv("results.csv")
+for year in years:
+    results = UILResults(contest, conference, meet_level, meet_level_num, year)
+    results.agg_data()
+    # results.print_results()
+    filename = ".\data\\" + str(year) + "_UIL_Results.csv"
+    results.write_csv(filename)
+    print(f"Done scraping {year}!")
